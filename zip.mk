055a2c82e84955dd4af3d6041ced8c23829cc304
Jakub Wilk <jwilk@jwilk.net>
2015-01-02 14:11:25 +0100

    Explode the makefile.

---
+++ zip.mk
@@ -0,0 +1,55 @@
+all: absolute.zip
+absolute.zip:
+	umask 022 && echo moo > mooo
+	zip -X $(@) mooo
+	rm -f mooo
+	sed -i -e s,mooo,/moo, $(@)
+
+all: absolute2.zip
+absolute2.zip:
+	umask 022 && echo moo > moooo
+	zip -X $(@) moooo
+	rm -f moooo
+	sed -i -e s,moooo,//moo, $(@)
+
+all: relative.zip
+relative.zip:
+	mkdir -m 755 tmp
+	umask 022 && echo moo > moo
+	cd tmp && zip -X ../$(@) ../moo
+	rm -rf tmp moo
+
+all: relative2.zip
+relative2.zip:
+	mkdir -p -m 755 tmp/tmp
+	umask 022 && echo moo > moo
+	cd tmp && zip -X ../$(@) tmp/../../moo
+	rm -rf tmp moo
+
+all: symlink.zip
+symlink.zip:
+	ln -sf /tmp/moo moo
+	zip -X -y $(@) moo
+	rm -f moo
+	umask 022 && echo moo > zoo
+	zip -X $(@) zoo
+	sed -i -e s/zoo/moo/ $(@)
+	rm -f zoo
+
+all: dirsymlink.zip
+dirsymlink.zip:
+	ln -sf /tmp tmp
+	zip -X -y $(@) tmp
+	rm -f tmp
+	mkdir -m 755 tmp
+	umask 022 && echo moo > tmp/moo
+	zip -X $(@) tmp/moo
+	rm -rf tmp
+
+clean: clean-zip
+
+.PHONY: clean-zip
+clean-zip:
+	rm -rf *.zip
+
+# vim:ts=4 sts=4 sw=4 noet
